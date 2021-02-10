de7f21a0edfee7fb14efb2078cba17d473991fbb
Kenny <koishybay.kenessary@targetai.kz>
2021-02-10 16:14:20 +0600

    time

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@
 
 int main() {
     auto cap = cv::VideoCapture("../data/videos/demo.mp4");
-
+    auto detector = Detector("efficientdet-d0_frozen");
 //    auto detector = EfficientDet("efficientdet-256x512");
 //    auto detector = EfficientDet("efficientdet-d0-512x512");
-    auto detector = EfficientDet("efficientdet-d0-384x384");
+//    auto detector = EfficientDet("efficientdet-d0-384x384");
     cv::Mat frame;
 
     float frame_n = 0;
@@ -25,7 +25,7 @@ int main() {
         detector.Predict(frame, dets);
         auto now = std::chrono::high_resolution_clock::now();
         std::chrono::milliseconds spend = std::chrono::duration_cast<std::chrono::milliseconds>(now - bf);
-//        std::cout << "\rSpeed in ms:" << spend.count();
-//        std::flush(std::cout);
+        std::cout << "\rSpeed in ms:" << spend.count();
+        std::flush(std::cout);
     }
 }
