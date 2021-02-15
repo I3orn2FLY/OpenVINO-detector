89fa3ae9745187a136ec9966dd8568bc630c4c75
Kenny <koishybay.kenessary@targetai.kz>
2021-02-15 14:01:27 +0600

    new efficient det

--- detection/efficientdet.cpp
+++ detection/efficientdet.cpp
@@ -7,16 +7,7 @@
 EfficientDet::EfficientDet(const std::string &model_name) : Detector(model_name) {
     auto shapes = net->GetOutputShapes();
 
-    idx2cls = {"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck",
-               "boat", "traffic light", "fire hydrant", "", "stop sign", "parking meter", "bench", "bird", "cat", "dog",
-               "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "", "backpack", "umbrella", "", "",
-               "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-               "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "", "wine glass", "cup", "fork",
-               "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog",
-               "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "", "dining table", "", "",
-               "toilet", "", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven",
-               "toaster", "sink", "refrigerator", "", "book", "clock", "vase", "scissors", "teddy bear",
-               "hair drier", "toothbrush"};
+    idx2cls = {"person", "car", ""};
 }
 
 float computeIOU(const Detection &box1, const Detection &box2) {
@@ -87,73 +78,20 @@ void EfficientDet::Predict(const cv::Mat &img, Detections &out_dets) {
 
     auto spend_nn = ST_GET_TIMESTAMP() - bf1;
 
-
     auto bf2 = ST_GET_TIMESTAMP();
     auto shapes = net->GetOutputShapes();
-
-    std::vector<std::vector<float>> ratios = {{1.f, 1.f},
-                                              {1.4, 0.7},
-                                              {0.7, 1.4}};
-    std::vector<float> scales = {
-            1,
-            1.2599210498948732,
-            1.5874010519681994
-    };
-
-    size_t n_cls = 90;
+    size_t n_cls = 3;
     size_t box_reg_n = 4;
-    auto reg_cell_size = (box_reg_n * scales.size() * ratios.size());
-    auto cls_cell_size = (n_cls * scales.size() * ratios.size());
-
-
-    for (size_t p = 0; p < 5; p++) {
-        auto *cls_res = net->GetOutput(p);
-        auto *reg_res = net->GetOutput(p + 5);
-        auto shape = net->GetOutputShape(p + 5);
-        auto rows = shape[1];
-        auto cols = shape[2];
-        float stride_y = 1.f / rows;
-        float stride_x = 1.f / cols;
-
-
-        for (size_t i = 0; i < rows; ++i) {
-            for (size_t j = 0; j < cols; ++j) {
-                auto *reg_cell = reg_res + (rows * i + j) * reg_cell_size;
-                auto *cls_cell = cls_res + (rows * i + j) * cls_cell_size;
-
-                for (size_t sc_idx = 0; sc_idx < scales.size(); sc_idx++) {
-                    for (size_t r_idx = 0; r_idx < ratios.size(); r_idx++) {
-                        auto reg_vals = reg_cell + (sc_idx * scales.size() + r_idx) * box_reg_n;
-                        auto cls_vals = cls_cell + (sc_idx * scales.size() + r_idx) * n_cls;
-
-                        auto p_max = std::max_element(cls_vals, cls_vals + n_cls);
-                        float score = *p_max;
-                        if (score < 0.3) {
-                            continue;
-                        }
-                        size_t cls_idx = std::distance(p_max, cls_vals);
-
-                        auto scale = scales[sc_idx];
-                        auto ratio = ratios[r_idx];
-                        float anchor_y = 4.f * scale * ratio[1] * stride_y;
-                        float anchor_x = 4.f * scale * ratio[0] * stride_x;
-                        float h = exp(reg_vals[2]) * anchor_y;
-                        float w = exp(reg_vals[3]) * anchor_x;
-                        float y_c = reg_vals[0] * anchor_y + (float(i) + 0.5f) * stride_y;
-                        float x_c = reg_vals[1] * anchor_x + (float(j) + 0.5f) * stride_x;
-
-                        float x0 = std::max(x_c - w / 2, 0.f);
-                        float y0 = std::max(y_c - h / 2, 0.f);
-                        float x1 = std::min(x_c + w / 2, 1.f);
-                        float y1 = std::min(y_c + h / 2, 1.f);
-
-                        auto det = Detection{score, x0, y0, x1, y1};
-                        out_dets.push_back(det);
-                    }
-                }
-
-            }
-        }
+    size_t num_proposals = net->GetOutputShape(0)[1];
+    auto *reg_res = net->GetOutput(0);
+    auto *cls_res = net->GetOutput(1);
+    for (size_t i = 0; i < num_proposals; ++i) {
+        auto box = reg_res + box_reg_n * i;
+        auto cls = cls_res + n_cls * i;
+        float score = std::max(cls[0], cls[1]);
+        if (score < 0.2) continue;
+        Detection det = {score, box[0] / inp_w, box[1] / inp_h, box[2] / inp_w, box[3] / inp_h};
+        out_dets.push_back(det);
     }
 
 
@@ -168,20 +106,19 @@ void EfficientDet::Predict(const cv::Mat &img, Detections &out_dets) {
         det.x1 = std::min(det.x1 / x_max, 1.f);
         det.y1 = std::min(det.y1 / y_max, 1.f);
 
-//        auto p0 = cv::Point2f{det.x0 * img.cols, det.y0 * img.rows};
-//        auto p1 = cv::Point2f{det.x1 * img.cols, det.y1 * img.rows};
+        auto p0 = cv::Point2f{det.x0 * img.cols, det.y0 * img.rows};
+        auto p1 = cv::Point2f{det.x1 * img.cols, det.y1 * img.rows};
 //
-//////        auto p0 = cv::Point2f{det.x0, det.y0};
-//////        auto p1 = cv::Point2f{det.x1, det.y1};
-//        cv::rectangle(img, p0, p1, {0, 255, 255}, 2);
+;
+        cv::rectangle(img, p0, p1, {0, 255, 255}, 2);
     }
 
-    auto spend_post = ST_GET_TIMESTAMP() - bf2;
-    auto spend_ovr = ST_GET_TIMESTAMP() - bf0;
-    std::cout << "\rSpend Preprocess:" << spend_pre << "      Spend NN:" << spend_nn << "     Spend POST:" << spend_post
-              << " Spend Overall:" << spend_ovr;
-    std::flush(std::cout);
-//    cv::imshow("Detector Out", img);
-//    cv::waitKey(0);
+//    auto spend_post = ST_GET_TIMESTAMP() - bf2;
+//    auto spend_ovr = ST_GET_TIMESTAMP() - bf0;
+//    std::cout << "\rSpend Preprocess:" << spend_pre << "      Spend NN:" << spend_nn << "     Spend POST:" << spend_post
+//              << " Spend Overall:" << spend_ovr;
+//    std::flush(std::cout);
+    cv::imshow("Detector Out", img);
+    cv::waitKey(1);
 //    std::cout << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -7,25 +7,25 @@
 
 int main() {
     auto cap = cv::VideoCapture("../data/videos/demo.mp4");
-    auto detector = Detector("efficientdet-d0_frozen");
+//    auto detector = Detector("efficientdet-post-proc");
+//    auto detector = Detector("efficientdet-d0_v0.0.1");
 //    auto detector = EfficientDet("efficientdet-256x512");
 //    auto detector = EfficientDet("efficientdet-d0-512x512");
-//    auto detector = EfficientDet("efficientdet-d0-384x384");
+    auto detector = EfficientDet("efficientdet-d0_v0.0.1");
     cv::Mat frame;
 
     float frame_n = 0;
     while (cap.read(frame)) {
         frame_n += 1;
         Detections dets;
-        auto bf = std::chrono::high_resolution_clock::now();
+        auto bf = ST_GET_TIMESTAMP();
 
 //        frame = cv::imread("../data/images/zidane.jpg");
 //        frame = cv::imread("../data/images/img.png");
 
         detector.Predict(frame, dets);
-        auto now = std::chrono::high_resolution_clock::now();
-        std::chrono::milliseconds spend = std::chrono::duration_cast<std::chrono::milliseconds>(now - bf);
-        std::cout << "\rSpeed in ms:" << spend.count();
+        auto spend = ST_GET_TIMESTAMP() - bf;
+        std::cout << "\rSpeed in ms:" << spend;
         std::flush(std::cout);
     }
 }
